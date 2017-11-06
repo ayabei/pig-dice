b79f8b50a00de36bf675ce004739fd693a7dc734
Ayabei <ayabeidoreen@gmail.com>
2017-11-06 08:55:18 +0300

    add a README.md file

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = Math.floor((Math.random() * 6) + 1);
+    var y = Math.floor((Math.random() * 6) + 1);
+    
+    
+    $("#output").append(x);
+
+    $("#output1").append(y);
+
+}
